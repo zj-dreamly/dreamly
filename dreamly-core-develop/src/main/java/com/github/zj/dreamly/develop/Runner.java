92257de3503d61429aa63ea068e4e0a8c8c37071
苍海之南 <1782920040@qq.com>
2019-08-07 11:15:25 +0800

    Fix：修正部分方法名

--- dreamly-core-develop/src/main/java/com/github/zj/dreamly/develop/Runner.java
+++ dreamly-core-develop/src/main/java/com/github/zj/dreamly/develop/Runner.java
@@ -34,7 +34,7 @@ public class Runner {
 	/**
 	 * 需要生成的表名(两者只能取其一)
 	 */
-	private static final String[] INCLUDE_TABLES = {"market_template","market_template_step"};
+	private static final String[] INCLUDE_TABLES = {"market_template_url"};
 	/**
 	 * 需要排除的表名(两者只能取其一)
 	 */
