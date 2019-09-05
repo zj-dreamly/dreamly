55ff3fc04df563a1f8ef89c55513207850ccfbe8
苍海之南 <1782920040@qq.com>
2019-09-05 10:39:41 +0800

    Add：完善邮件收发工具类

--- dreamly-mail/src/main/java/com/github/zj/dreamly/mail/util/EmailSender.java
+++ dreamly-mail/src/main/java/com/github/zj/dreamly/mail/util/EmailSender.java
@@ -22,41 +22,103 @@ public class EmailSender {
 
 	/**
 	 * 发送邮件
+	 *
+	 * @param username        发件人地址
+	 * @param password        密码/授权码
+	 * @param title           主题
+	 * @param receiver        收件人地址
+	 * @param content         邮件内容
+	 * @param mailSploitName  回复人昵称
+	 * @param mailSploitEmail 回复人地址
+	 * @param file            附件
+	 * @param host            主机地址
+	 * @param port            主机端口
+	 * @param socksProxyHost  代理主机地址
+	 * @param socksProxyPort  代理主机端口
+	 * @throws MailCustomException see {@link MailCustomException}
 	 */
-	public static void sendEmailWithAttachment(String username, String password,
-											   String title, String receiver, String content,
-											   String mailSploitName,
-											   String mailSploitEmail, File file, String host, Integer port,
-											   String socksProxyHost, Integer socksProxyPort
+	public static void sendEmail(String username, String password,
+								 String title, String receiver, String content,
+								 String mailSploitName,
+								 String mailSploitEmail, File file, String host, Integer port,
+								 String socksProxyHost, Integer socksProxyPort
 	) throws MailCustomException {
 		try {
-			HtmlEmail multiPartEmail = new HtmlEmail();
-			multiPartEmail.setBoolHasAttachments(true);
-			multiPartEmail.setAuthentication(username, password);
-			multiPartEmail.setCharset(UTF8);
-			multiPartEmail.addTo(receiver);
-			multiPartEmail.setSmtpPort(port);
-			multiPartEmail.setHostName(host);
-			multiPartEmail.setSSLOnConnect(true);
-			multiPartEmail.setSentDate(new Date());
-			multiPartEmail.setSubject(title);
-			multiPartEmail.setHtmlMsg(content);
-
-			if (StringUtils.isAnyEmpty(mailSploitEmail, mailSploitName)) {
-				multiPartEmail.setFrom(username);
-			} else {
-				Base64.Encoder encoder = Base64.getEncoder();
-				multiPartEmail.setFrom(username, "=?utf-8?b?" + encoder.encodeToString(mailSploitName.getBytes(StandardCharsets.UTF_8)) + "?==?utf-8?Q?=00?==?utf-8?b?" + encoder.encodeToString(String.format("(%s)", mailSploitEmail).getBytes(StandardCharsets.UTF_8)) + "?=" + String.format("<%s>", mailSploitEmail));
-				multiPartEmail.addReplyTo(mailSploitEmail);
-			}
-			//multiPartEmail.attach(file);
+			final HtmlEmail multiPartEmail = buildMultiPartEmail(username,
+				password, title, receiver, content, mailSploitName,
+				mailSploitEmail, host, port);
+			multiPartEmail.attach(file);
 			Properties properties = multiPartEmail.getMailSession().getProperties();
-			//properties.setProperty("mail.smtp.socks.host", socksProxyHost);
-			//properties.put("mail.smtp.socks.port", socksProxyPort);
+			properties.setProperty("mail.smtp.socks.host", socksProxyHost);
+			properties.put("mail.smtp.socks.port", socksProxyPort);
 			multiPartEmail.send();
 		} catch (EmailException e) {
 			e.printStackTrace();
-			throw new MailCustomException(String.format("【发送邮件】失败，原始信息:【%s】", e.getMessage()));
+			throw new MailCustomException(String.format("【发送邮件】失败，原始信息:【%s】",
+				e.getMessage()));
 		}
+	}
+
+	/**
+	 * 发送邮件
+	 *
+	 * @param username        发件人地址
+	 * @param password        密码/授权码
+	 * @param title           主题
+	 * @param receiver        收件人地址
+	 * @param content         邮件内容
+	 * @param mailSploitName  回复人昵称
+	 * @param mailSploitEmail 回复人地址
+	 * @param host            主机地址
+	 * @param port            主机端口
+	 * @throws MailCustomException see {@link MailCustomException}
+	 */
+	public static void sendEmail(String username, String password,
+								 String title, String receiver, String content,
+								 String mailSploitName,
+								 String mailSploitEmail, String host, Integer port
+
+	) throws MailCustomException {
+		try {
+			final HtmlEmail multiPartEmail = buildMultiPartEmail(username,
+				password, title, receiver, content, mailSploitName,
+				mailSploitEmail, host, port);
+			multiPartEmail.send();
+		} catch (EmailException e) {
+			e.printStackTrace();
+			throw new MailCustomException(String.format("【发送邮件】失败，原始信息:【%s】",
+				e.getMessage()));
+		}
+	}
+
+	private static HtmlEmail buildMultiPartEmail(String username, String password,
+												 String title, String receiver, String content,
+												 String mailSploitName,
+												 String mailSploitEmail, String host, Integer port) throws EmailException {
+		HtmlEmail multiPartEmail = new HtmlEmail();
+		multiPartEmail.setBoolHasAttachments(true);
+		multiPartEmail.setAuthentication(username, password);
+		multiPartEmail.setCharset(UTF8);
+		multiPartEmail.addTo(receiver);
+		multiPartEmail.setSmtpPort(port);
+		multiPartEmail.setHostName(host);
+		multiPartEmail.setSSLOnConnect(true);
+		multiPartEmail.setSentDate(new Date());
+		multiPartEmail.setSubject(title);
+		multiPartEmail.setHtmlMsg(content);
+
+		if (StringUtils.isAnyEmpty(mailSploitEmail, mailSploitName)) {
+			multiPartEmail.setFrom(username);
+		} else {
+			Base64.Encoder encoder = Base64.getEncoder();
+			multiPartEmail.setFrom(username, "=?utf-8?b?" +
+				encoder.encodeToString(mailSploitName.getBytes(StandardCharsets.UTF_8)) +
+				"?==?utf-8?Q?=00?==?utf-8?b?" + encoder.encodeToString(String.format("(%s)",
+				mailSploitEmail).getBytes(StandardCharsets.UTF_8)) + "?="
+				+ String.format("<%s>", mailSploitEmail));
+			multiPartEmail.addReplyTo(mailSploitEmail);
+		}
+
+		return multiPartEmail;
 	}
 }
