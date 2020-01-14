1a49a3c1b7d53252e216e402fcb7e47c1aeb9554
苍海之南 <1782920040@qq.com>
2020-01-14 11:23:28 +0800

    Fix：tools默认支持跨域配置

---
+++ dreamly-starter-tool/src/main/java/com/github/zj/dreamly/tool/exception/CustomCorsConfiguration.java
@@ -0,0 +1,28 @@
+package com.github.zj.dreamly.tool.exception;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author 苍海之南
+ */
+@Configuration
+public class CustomCorsConfiguration {
+	private CorsConfiguration buildConfig() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.addAllowedOrigin("*");
+		corsConfiguration.addAllowedHeader("*");
+		corsConfiguration.addAllowedMethod("*");
+		return corsConfiguration;
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", buildConfig());
+		return new CorsFilter(source);
+	}
+}
