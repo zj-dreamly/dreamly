52ca601d80303ceb788102ac2a2b5573e9edf349
苍海之南 <1782920040@qq.com>
2020-01-17 12:59:18 +0800

    Fix：移除部分无用模块

---
+++ dreamly-starter-tool/src/main/java/com/github/zj/dreamly/tool/configuration/RequestFilter.java
@@ -0,0 +1,49 @@
+package com.github.zj.dreamly.tool.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * <h2>RequestFilter</h2>
+ *
+ * @author: 苍海之南
+ * @since: 2019-09-19 10:27
+ **/
+@Slf4j
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class RequestFilter extends GenericFilterBean {
+
+    private static final String PREFIX = "/webjars";
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+						 FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        final String uri = request.getRequestURI();
+        if (uri.startsWith(PREFIX)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final long startTime = System.currentTimeMillis();
+        filterChain.doFilter(request, response);
+        log.info("【本次请求地址】：{}|{}, 【耗时】：{}ms", request.getMethod(), request.getRequestURI(),
+                System.currentTimeMillis() - startTime);
+
+    }
+}
