3e449da5119381fe4849e33bbabafe7147dc4e00
苍海之南 <1782920040@qq.com>
2020-08-14 11:53:03 +0800

    Add: webim sdk

---
+++ dreamly-starter-im/src/main/java/com/github/zj/dreamly/im/coder/WebMessageEncoder.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2013-2019 Xia Jun(3979434@qq.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ***************************************************************************************
+ *                                                                                     *
+ *                        Website : http://www.farsunset.com                           *
+ *                                                                                     *
+ ***************************************************************************************
+ */
+package com.github.zj.dreamly.im.coder;
+
+import com.github.zj.dreamly.im.model.Transportable;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.UnpooledByteBufAllocator;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageEncoder;
+import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
+
+import java.util.List;
+
+/**
+ * websocket发送消息前编码
+ */
+public class WebMessageEncoder extends MessageToMessageEncoder<Transportable> {
+
+	private static final UnpooledByteBufAllocator BYTE_BUF_ALLOCATOR = new UnpooledByteBufAllocator(false);
+
+	@Override
+	protected void encode(ChannelHandlerContext ctx, Transportable data, List<Object> out) {
+		byte[] body = data.getBody();
+		ByteBuf buffer = BYTE_BUF_ALLOCATOR.buffer(body.length + 1);
+		buffer.writeByte(data.getType());
+		buffer.writeBytes(body);
+		out.add(new BinaryWebSocketFrame(buffer));
+	}
+}
